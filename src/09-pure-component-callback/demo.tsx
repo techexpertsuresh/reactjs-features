b942ed3085fd6c585a0fce7e402af18ac463e7a2
Suresh kumar Iyyappan <sureshkumariyyappan@sureshs-air.kpn>
2023-01-06 03:52:24 +0530

    reactjs features

---
+++ src/09-pure-component-callback/demo.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+interface Props {
+  onReset: () => void;
+}
+
+const ResetValue: React.FC<Props> = React.memo((props) => {
+  console.log(
+    "Hey I'm only rendered the first time, check React.memo + callback"
+  );
+
+  return <button onClick={props.onReset}>Reset value</button>;
+});
+
+export const MyComponent = () => {
+  const [username, setUsername] = React.useState("John");
+  const [lastname, setLastname] = React.useState("Doe");
+
+  const resetAllFields = () => {
+    setUsername("");
+    setLastname("");
+  }
+  const resetNameCallback = React.useCallback(() => resetAllFields(), []);
+
+  return (
+    <>
+      <h3>
+        {username} {lastname}
+      </h3>
+      <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input value={lastname} onChange={(e) => setLastname(e.target.value)} />
+      <ResetValue onReset={resetNameCallback}>Reset name</ResetValue>
+    </>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./00-boilerplate/app";
+//import { App } from "./01-use-state/app";
+//import { App } from "./02-use-state-object/app";
+//import { App } from "./03-component-did-onload/app";
+//import { App } from "./04-component_unmount/app";
+//import { App } from "./05-component-update-render/app";
+//import { App } from "./06-ajax-field-change/app";
+//import { App } from "./07-custom-hook/app";
+//import { App } from "./08-pure-component/app";
+//import { App } from "./09-pure-component-callback/app";
+//import { App } from "./10-use-reducer/app";
+//import { App } from "./11-use-context/app";
+//import { App } from "./12-set-state-func/app";
+//import { App } from "./13-async-closure/app";
+//import { App } from "./14-use-ref-dom/app";
+//import { App } from "./15-promise-unmounted/app";
+//import { App } from "./16-memo-predicate/app";
+//import { App } from "./17-use-debug-value/app";
+//import { App } from "./18-why-did-you-update/app";
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
